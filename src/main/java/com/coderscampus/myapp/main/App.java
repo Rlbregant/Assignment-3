f1ce56015529dbc3263b2142ac07a3f88b336594
Ross <23426097+Rlbregant@users.noreply.github.com>
2022-11-28 10:51:41 -0700

    1

--- src/main/java/com/coderscampus/myapp/main/App.java
+++ src/main/java/com/coderscampus/myapp/main/App.java
@@ -2,33 +2,39 @@ package com.coderscampus.myapp.main;
 
 import java.util.Scanner;
 
-
 public class App {
 
 	public static void main(String[] args) {
-		new App().execute();	// Moving out of Static methods
-		
+		new App().execute(); // Moving out of Static methods
+
 	}
 
 	private void execute() {
 
-		
-			UserService userService = new UserService(); // Instantiating UserService
-			userService.parseText(); // Running the method parseText in UserService 
-		
-
+		UserService userService = new UserService(); // Instantiating UserService
+		userService.parseText(); // Running the method parseText in UserService
 		// User input block with my test to make sure it worked
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.println("Enter your email:");
-//		String email = scanner.nextLine();
-//		System.out.println("Enter your password:");
-//		String password = scanner.nextLine();
-//		
-//		System.out.println(email + " " + password);
-//		scanner.close();
-		
-		
+		// Now a while look with 5 login attempts and user input
+		int attemptsRemaining = 5;
+		while (attemptsRemaining > 0) {
+			attemptsRemaining--;
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter your email:");
+			String email = scanner.nextLine();
+			System.out.println("Enter your password:");
+			String password = scanner.nextLine();
+
+			System.out.println(email + " " + password); // This needs changed
+
+			if (attemptsRemaining == 0) {
+				scanner.close();
+				System.out.println("Too many failed login attempts, you are now locked out.");
+
+			} else if(email =  true && password = true){
+				System.out.println("Welcome " );
+			}
+
+		}
 
 	}
-
 }
