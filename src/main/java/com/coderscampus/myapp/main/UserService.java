a67dde65050e3bd79a43ef4243e53cec535e368c
Ross <23426097+Rlbregant@users.noreply.github.com>
2022-11-29 15:12:42 -0700

    brackets finder

--- src/main/java/com/coderscampus/myapp/main/UserService.java
+++ src/main/java/com/coderscampus/myapp/main/UserService.java
@@ -15,19 +15,20 @@ public class UserService {
 		Scanner scanner = new Scanner(System.in);
 		BufferedReader buff = null; // Not sure why they wanted to move this up here or change it to null
 		List<User> listOfUser = new ArrayList<>(); // Instantiating an Arraylist of type String
+		try {
+			buff = new BufferedReader(new FileReader("data.txt"));
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
 		try { // Try block which comes before a catch block for possible errors
-			try {
-				buff = new BufferedReader(new FileReader("data.txt"));
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 
 			while (buff != null) { // a while loop that stops when BufferedReader hits an empty line.
 
 				String line;
 				
-				try {
+				
 					for (int i = 0; i > 3; i++) {
 						line = buff.readLine();
 						String[] array = line.split(","); // Splitting the line by the commas to give 0 - 2 Array slots
@@ -37,6 +38,7 @@ public class UserService {
 						User user = new User(userEmail, userPassword, userName);
 						listOfUser.add(user);
 					}
+				
 					while (attemptsRemaining > 0) {
 						attemptsRemaining--;
 						System.out.println("Enter your email:");
@@ -44,51 +46,51 @@ public class UserService {
 						System.out.println("Enter your password:");
 						String password = scanner.nextLine();
 
-						if (!User.getuserEmail().equalsIgnoreCase(email)
-								&& !user.getuserPassword().equalsIgnoreCase(password))
-							System.out.println("Invalid login, please try again");
-
-						if (attemptsRemaining == 0) {
-							scanner.close();
-							System.out.println("Too many failed login attempts, you are now locked out.");
-
-						} else if (user.getuserEmail().equalsIgnoreCase(email)
-								&& user.getuserPassword().equalsIgnoreCase(password)) {
+						if (user.getuserEmail().equalsIgnoreCase(email)
+								&& user.getuserPassword().equals(password)) {
 							System.out.println("Welcome " + user.getuserName());
-							break;
+						
+				
+						else {
+							if (attemptsRemaining == 0) {
+								scanner.close();
+								System.out.println("Too many failed login attempts, you are now locked out.");
+								break;
+						} else 
+							System.out.println("Invalid login, please try again");
+						
 						}
 
 					}
 					scanner.close();
 
-				} catch (IOException e) {
-
-					e.printStackTrace();
-				} // Reads a line of text and saves it as variable line
+				 
+				 // Reads a line of text and saves it as variable line
 //				listOfStrings.add(line);		// Puts line in ArrayList called listOfStrings
-
+					}
 			}
-
-		} finally {
+					 catch (Exception e) {
+						System.out.println("Error - " + e.toString());
+					} finally {
+						buff.close();
+						System.out.println(listOfUser);
+					}
+			
+					 
+		 
 			// It's the thing that runs after the loop finishes
-			try {
-				buff.close(); // Closes BufferedReader to prevent memory leaks
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			System.out.println(listOfUser);
-		}
-
+						
+		
+		
 //		} catch (IOException e) { // The Catch block to not have runtime errors and disrupt the program
 //			System.out.println("Error - " + e.toString());
 //		} catch (NullPointerException a) {
 //			System.out.println("Error - " + a.toString());
 //		} 
 
-		return null; // The method isn't void so it must return something. In this case its returning
-						// null
-
+		
+		
+	
+	
 	}
-
 }
